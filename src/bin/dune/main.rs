4f81a1305eb6a10ce60a87ed9fd3744ec1f44e3e
Zelda Hessler <zelda.hessler@pm.me>
2021-08-03 14:15:39 -0500

    add: dune ui

---
+++ src/bin/dune/main.rs
@@ -0,0 +1,324 @@
+mod triangle;
+
+use crate::triangle::new_triangles_from_noise;
+use anyhow::Context;
+use chrono::Local;
+use log::{debug, error, info, trace, warn};
+use nannou::{prelude::*, ui::prelude::*};
+use std::path::PathBuf;
+use triangle::{bounding_rect_from_triangles, TriangleParams, Triangles};
+
+fn main() {
+    let res = dotenv::dotenv();
+    env_logger::init();
+    if let Err(err) = res {
+        warn!("{}", err)
+    };
+
+    nannou::app(model).update(update).run();
+}
+
+#[derive(Default)]
+struct State {
+    pub triangles: Triangles,
+    pub triangle_params: TriangleParams,
+    pub height: f32,
+    pub width: f32,
+    pub show_viewbox: bool,
+    pub should_rebuild: bool,
+}
+
+impl State {
+    fn new(width: f32, height: f32) -> Self {
+        Self {
+            triangles: Default::default(),
+            triangle_params: Default::default(),
+            height,
+            width,
+            show_viewbox: false,
+            should_rebuild: true,
+        }
+    }
+
+    fn update(&mut self) {
+        // do nothing
+    }
+}
+
+struct Model {
+    ui: Ui,
+    ids: Ids,
+    pub state: State,
+}
+
+widget_ids! {
+    struct Ids {
+        export_svg,
+        max_height,
+        min_height,
+        noise_scale,
+        noise_seed,
+        param_title_text,
+        skew,
+        toggle_viewbox,
+        triangle_count,
+        vertical_spacing,
+        wh_ratio,
+    }
+}
+
+fn model(app: &App) -> Model {
+    // Set the loop mode to wait for events, an energy-efficient option for pure-GUI apps.
+    app.set_loop_mode(LoopMode::Wait);
+    let (width, height) = (850, 1100);
+
+    let window_id = app
+        .new_window()
+        .size(width, height)
+        .view(view)
+        .build()
+        .expect("couldn't create a window");
+
+    // Create the UI.
+    let mut ui = app.new_ui().window(window_id).build().unwrap();
+
+    // Generate some ids for our widgets.
+    let ids = Ids::new(ui.widget_id_generator());
+    let state = State::new(width as f32, height as f32);
+
+    Model { ui, ids, state }
+}
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    update_ui(model);
+    model.state.update();
+
+    if model.state.should_rebuild {
+        model.state.triangles = new_triangles_from_noise(&model.state.triangle_params);
+        model.state.should_rebuild = false;
+        trace!("State is has not been initialized, doing so now...");
+    }
+}
+
+const SETTING_WIDTH: f64 = 300.0;
+const SETTING_HEIGHT: f64 = 30.0;
+const SETTING_MARGIN: f64 = 10.0;
+const LABEL_COLOR: nannou::ui::color::Color = nannou::ui::color::WHITE;
+const FILL: nannou::ui::color::Color = nannou::ui::color::DARK_BLUE;
+const FONT_SIZE: u32 = 12;
+
+fn dialer(val: f32, min: f32, max: f32) -> widget::NumberDialer<'static, f32> {
+    use nannou::ui::color::*;
+    widget::NumberDialer::new(val, min, max, 0)
+        .w_h(SETTING_WIDTH, SETTING_HEIGHT)
+        .label_font_size(FONT_SIZE)
+        .color(FILL)
+        .label_color(LABEL_COLOR)
+        .border(0.0)
+}
+
+fn slider(val: f32, min: f32, max: f32) -> widget::Slider<'static, f32> {
+    use nannou::ui::color::*;
+    widget::Slider::new(val, min, max)
+        .w_h(SETTING_WIDTH, SETTING_HEIGHT)
+        .label_font_size(FONT_SIZE)
+        .color(FILL)
+        .label_color(LABEL_COLOR)
+        .border(0.0)
+}
+
+fn update_ui(model: &mut Model) {
+    let ui = &mut model.ui.set_widgets();
+    let mut should_refresh_model = false;
+
+    widget::Text::new("Parameters")
+        .top_left_with_margin(20.0)
+        .color(LABEL_COLOR)
+        .font_size(20)
+        .set(model.ids.param_title_text, ui);
+
+    for count in dialer(model.state.triangle_params.count as f32, 1.0, 512.0)
+        .down(SETTING_MARGIN)
+        .label("Count")
+        .set(model.ids.triangle_count, ui)
+    {
+        model.state.triangle_params.count = count.floor() as u32;
+        should_refresh_model = true;
+    }
+
+    for skew in slider(model.state.triangle_params.skew, 0.0, 1.0)
+        .down(SETTING_MARGIN)
+        .label("Skew")
+        .set(model.ids.skew, ui)
+    {
+        model.state.triangle_params.skew = skew;
+        should_refresh_model = true;
+    }
+
+    for vertical_spacing in slider(model.state.triangle_params.vertical_spacing, 1.0, 8.0)
+        .down(SETTING_MARGIN)
+        .label("Vertical Spacing")
+        .set(model.ids.vertical_spacing, ui)
+    {
+        model.state.triangle_params.vertical_spacing = vertical_spacing;
+        should_refresh_model = true;
+    }
+
+    for wh_ratio in slider(model.state.triangle_params.wh_ratio, 0.5, 3.14)
+        .down(SETTING_MARGIN)
+        .label("Width/Height Ratio")
+        .set(model.ids.wh_ratio, ui)
+    {
+        model.state.triangle_params.wh_ratio = wh_ratio;
+        should_refresh_model = true;
+    }
+
+    for noise_seed in slider(model.state.triangle_params.noise_seed as f32, -1.0, 1.0)
+        .down(SETTING_MARGIN)
+        .label("Noise")
+        .set(model.ids.noise_seed, ui)
+    {
+        model.state.triangle_params.noise_seed = noise_seed as f64;
+        should_refresh_model = true;
+    }
+
+    for noise_scale in slider(model.state.triangle_params.noise_scale as f32, -0.06, 0.06)
+        .down(SETTING_MARGIN)
+        .label("Noise Scale")
+        .set(model.ids.noise_scale, ui)
+    {
+        model.state.triangle_params.noise_scale = noise_scale as f64;
+        should_refresh_model = true;
+    }
+
+    for _click in widget::Button::new()
+        .down(SETTING_MARGIN)
+        .w_h(SETTING_WIDTH, SETTING_HEIGHT)
+        .label_font_size(FONT_SIZE)
+        .color(FILL)
+        .label_color(LABEL_COLOR)
+        .border(0.0)
+        .label("Toggle Viewbox")
+        .set(model.ids.toggle_viewbox, ui)
+    {
+        model.state.show_viewbox = !model.state.show_viewbox;
+        should_refresh_model = true;
+    }
+
+    for _click in widget::Button::new()
+        .down(SETTING_MARGIN)
+        .w_h(SETTING_WIDTH, SETTING_HEIGHT)
+        .label_font_size(FONT_SIZE)
+        .color(FILL)
+        .label_color(LABEL_COLOR)
+        .border(0.0)
+        .label("Export SVG")
+        .set(model.ids.export_svg, ui)
+    {
+        if let Err(err) = export_as_svg(&model.state) {
+            error!("{}", err)
+        }
+    }
+
+    if should_refresh_model {
+        model.state.should_rebuild = true;
+        trace!("refresh model called");
+    }
+}
+
+// Draw the state of your `Model` into the given `Frame` here.
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(WHITE);
+
+    let dune_bounding_rect = bounding_rect_from_triangles(&model.state.triangles)
+        .align_middle_x_of(app.window_rect())
+        .align_middle_y_of(app.window_rect())
+        .shift_y(model.state.height / 4.0);
+
+    if model.state.show_viewbox {
+        draw.rect()
+            .xy(dune_bounding_rect.xy())
+            .wh(dune_bounding_rect.wh())
+            .stroke(RED)
+            .stroke_weight(2.0)
+            .no_fill();
+    }
+
+    let triangle_draw = draw.translate(dune_bounding_rect.xy().extend(0.0));
+
+    model
+        .state
+        .triangles
+        .iter()
+        .for_each(|triangle| triangle.draw(&triangle_draw));
+
+    // Write the result of our drawing to the window's frame.
+    draw.to_frame(app, &frame).unwrap();
+
+    // Draw the state of the `Ui` to the frame.
+    model.ui.draw_to_frame(app, &frame).unwrap();
+}
+
+fn build_svg_document_from_state(state: &State) -> svg::Document {
+    let doc = svg::Document::new().set("viewBox", (0, 0, state.width, state.height));
+
+    let mut group = svg::node::element::Group::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 1);
+
+    for triangle in state.triangles.iter() {
+        let path = triangle.as_svg();
+
+        group = group.add(path);
+    }
+
+    let bounding_rect = svg::node::element::Rectangle::new()
+        .set("width", state.width)
+        .set("height", state.height)
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 1);
+
+    doc.add(group).add(bounding_rect)
+}
+
+fn export_as_svg(state: &State) -> Result<(), anyhow::Error> {
+    info!("exporting image as SVG...");
+    let document = build_svg_document_from_state(state);
+    let base_path = std::env::var("SVG_EXPORT_DIRECTORY").context("setting 'SVG_EXPORT_DIRECTORY' is required, please set it to the directory you wish to export SVGs to")?;
+    let current_date = Local::today().format("%Y-%m-%d");
+    let svg_filename = format!("{}-plotling.svg", &current_date);
+    let mut svg_filepath: PathBuf = [base_path, svg_filename].iter().collect();
+
+    // I don't want to silently overwrite anything so I look for an unused filename,
+    // incrementing the counter until I find an unused number
+    // I could have also used a random string/number, I just like this better
+    if svg_filepath.exists() {
+        let mut counter = 1;
+
+        while svg_filepath.exists() {
+            if counter > 100 {
+                debug!(
+                    "export_as_svg counter has reached {}, you're not in an infinite loop are you?",
+                    counter
+                );
+            }
+
+            let _ = svg_filepath.pop();
+            let svg_filename = format!("{}-plotling-{}.svg", &current_date, &counter);
+            svg_filepath.push(svg_filename);
+            counter += 1;
+        }
+    }
+
+    svg::save(&svg_filepath, &document)?;
+    info!(
+        "SVG successfully exported to {}",
+        &svg_filepath.to_string_lossy()
+    );
+
+    Ok(())
+}
---
+++ src/bin/dune/triangle.rs
@@ -0,0 +1,216 @@
+use nannou::prelude::*;
+use noise::{NoiseFn, SuperSimplex};
+use svg::node::element::Polyline;
+
+pub struct Triangle {
+    points: [Point2; 3],
+    // /// The position of a triangle
+    // xy: Point2,
+    /*
+    |\                   /\                   /|
+    | \                 /  \                 / |
+    |  \      ->       /    \     ->        /  |
+    |___\             /______\             /___|
+
+    right-angle,    equilateral     right-angle,
+    left aligned                  right aligned
+    0.0      ->        skew       ->        1.0
+    */
+    // /// Controls whether a triangle leans to the left or to the right. See the definition of this struct for a diagram
+    // skew: f32,
+    // width: f32,
+    // height: f32,
+}
+
+impl Triangle {
+    pub fn new(xy: Vec2, skew: f32, width: f32, height: f32) -> Self {
+        let points = generate_triangle_points_from_attributes(xy, skew, width, height);
+
+        Self { points }
+    }
+
+    pub fn draw(&self, draw: &Draw) {
+        let points = self.points.iter().cloned();
+
+        draw.polyline()
+            .weight(1.0)
+            .caps_round()
+            .points_closed(points);
+    }
+
+    pub fn bounding_rect(&self) -> nannou::geom::Rect {
+        let (l, r, t, b) = self.points.iter().fold(
+            (f32::MAX, f32::MIN, f32::MAX, f32::MIN),
+            |(mut l, mut r, mut b, mut t), xy| {
+                if xy.x < l {
+                    l = xy.x;
+                };
+                if xy.x > r {
+                    r = xy.x;
+                };
+                if xy.x < b {
+                    b = xy.x;
+                };
+                if xy.y > t {
+                    t = xy.y;
+                };
+
+                (l, r, b, t)
+            },
+        );
+
+        let (a, b) = (vec2(l, t), vec2(r, b));
+
+        nannou::geom::Rect::from_corners(a, b)
+    }
+
+    pub fn as_svg(&self) -> Polyline {
+        let data: Vec<_> = self
+            .points
+            .iter()
+            .map(|p| format!("{:.2},{:.2}", p.x, p.y))
+            .collect();
+
+        Polyline::new().set("points", data.join(" "))
+    }
+}
+
+fn generate_triangle_points_from_attributes(
+    xy: Vec2,
+    skew: f32,
+    width: f32,
+    height: f32,
+) -> [Vec2; 3] {
+    let (origin_x, origin_y) = (xy.x - width / 2.0, xy.y - height / 2.0);
+
+    [
+        Vec2::new(origin_x, origin_y),
+        Vec2::new(origin_x + (width * skew), origin_y + height),
+        Vec2::new(origin_x + width, origin_y),
+    ]
+}
+
+pub type Triangles = Vec<Triangle>;
+
+pub fn bounding_rect_from_triangles(triangles: &[Triangle]) -> nannou::geom::Rect {
+    let (l, r, t, b) = triangles
+        .iter()
+        .map(|tri| tri.bounding_rect())
+        //       Left      Right     Bottom    Top
+        .fold(
+            (f32::MAX, f32::MIN, f32::MAX, f32::MIN),
+            |(mut l, mut r, mut b, mut t), tri_rect| {
+                let (tri_l, tri_r, tri_b, tri_t) = tri_rect.l_r_b_t();
+
+                if tri_l < l {
+                    l = tri_l;
+                };
+                if tri_r > r {
+                    r = tri_r;
+                };
+                if tri_b < b {
+                    b = tri_b;
+                };
+                if tri_t > t {
+                    t = tri_t;
+                };
+
+                (l, r, b, t)
+            },
+        );
+    let (a, b) = (vec2(l, t), vec2(r, b));
+
+    nannou::geom::Rect::from_corners(a, b)
+}
+
+pub fn new_triangles_from_noise(params: &TriangleParams) -> Triangles {
+    let noise_fn = SuperSimplex::new();
+    let halfway_point = (params.count / 2) as f32;
+    (0..=params.count)
+        .map(|index| {
+            let index = index as f32;
+            let count = params.count as f32;
+            let t = if index < halfway_point {
+                (index / count) * 2.0
+            } else {
+                ((count - index) / count) * 2.0
+            };
+            let x = (noise_fn.get([params.noise_seed, index as f64 * params.noise_scale]) * 200.0)
+                as f32;
+            let xy = pt2(x, index * -params.vertical_spacing);
+            let height = map_range(t, 0.0, 1.0, params.min_height, params.max_height);
+            let width = height * params.wh_ratio;
+
+            Triangle::new(xy, params.skew, width, height)
+        })
+        .collect()
+}
+
+// pub fn new_triangles_from_parabola(
+//     params: &TriangleParams,
+//     origin: Vec2,
+//     a: f32,
+//     b: f32,
+//     c: f32,
+// ) -> Triangles {
+//     if params.count == 0 {
+//         Vec::new()
+//     } else {
+//         let (starting_x, ending_x) = quadratic_formula(a, b, c);
+//         (0..=params.count)
+//             .map(|index| {
+//                 let t = index as f32 / params.count as f32;
+//                 let par_x = starting_x.interpolate(ending_x, t);
+//                 let size = a * par_x.exp2() + b * par_x + c;
+//
+//                 let xy = pt2(0.0, index as f32 * -params.vertical_spacing);
+//
+//                 Triangle::new(
+//                     xy + origin,
+//                     params.skew,
+//                     size * params.scale * params.wh_ratio,
+//                     size * params.scale,
+//                 )
+//             })
+//             .collect()
+//     }
+// }
+//
+// fn quadratic_formula(a: f32, b: f32, c: f32) -> (f32, f32) {
+//     assert_ne!(a, 0.0, "not a quadratic equasion");
+//
+//     let delta = (b.powi(2) - (4.0 * a * c)).sqrt();
+//     let x1 = (-b + delta) / (2.0 * a);
+//     let x2 = (-b - delta) / (2.0 * a);
+//
+//     (x1, x2)
+// }
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TriangleParams {
+    pub count: u32,
+    pub wh_ratio: f32,
+    pub skew: f32,
+    pub vertical_spacing: f32,
+    pub min_height: f32,
+    pub max_height: f32,
+    pub noise_seed: f64,
+    pub noise_scale: f64,
+}
+
+impl Default for TriangleParams {
+    fn default() -> Self {
+        Self {
+            count: 120,
+            skew: 0.5,
+            vertical_spacing: 5.0,
+            max_height: 200.0,
+            min_height: 10.0,
+            wh_ratio: 1.61803398875,
+            noise_seed: 0.0,
+            noise_scale: 0.02,
+        }
+    }
+}
+
+// <polygon points="0,100 50,25 50,75 100,0" />
